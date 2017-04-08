707da8dcf3220b4d85c0eddaa0ad75959eebd3f9
Nguyen Tu <tuunguyen2795@gmail.com>
2017-04-08 09:51:39 +0700

    Fixed some bug

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,8 @@ class Post < ActiveRecord::Base
 	has_many :post_categories
 	has_many :categories, through: :post_categories
 
-	validates :title, length: { in: 4..50 }, presence: true, uniqueness: { case_sensitive: false }
-	validates :description, length: { in: 10..500 }, presence: true
+	validates :title, length: { in: 4..100 }, presence: true, uniqueness: { case_sensitive: false }
+	validates :description, minimum: 50, presence: true
 	#validates :user_id, presence: true
 
 end
